ea3e05a0e9c377c17736a4d40f2eb5c3bf8f1830
Jay <756774388@qq.com>
2021-08-26 14:51:07 +0800

    新增计数器demo

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
+  state() {
+    return {
+      count: 0
+    }
   },
   mutations: {
+    increment(state) {
+      state.count++
+    },
+    minus(state) {
+      state.count--
+    }
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      计数器
+      {{ this.$store.state.count }}
+      <el-button @click="increment" type="primary">+</el-button>
+      <el-button @click="minus" type="success">-</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+    minus() {
+      this.$store.commit("minus");
+    },
+  },
+};
 </script>
