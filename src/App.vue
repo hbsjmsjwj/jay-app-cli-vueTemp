a13fed47d58c01d7bbea11cb1c9037b55c41d4c0
Jay <756774388@qq.com>
2021-08-26 14:10:16 +0800

    新增layout

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,119 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div> -->
+  <el-container>
+    <el-header>
+      <el-avatar :size="size" :src="avatarUrl"></el-avatar>
+      <div class="title">jay-app-cli</div>
+    </el-header>
     <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>
-        <router-view/>
-      </el-main>
-      <el-footer>Footer</el-footer>
+      <el-aside width="200px">
+        <el-menu
+          :uniqueOpened="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template #title>分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <template #title>导航二</template>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <template #title>导航三</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <template #title>导航四</template>
+          </el-menu-item>
+          <el-sub-menu index="5">
+            <template #title>
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template #title>分组一</template>
+              <el-menu-item index="5-1">选项1</el-menu-item>
+              <el-menu-item index="5-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="5-3">选项3</el-menu-item>
+            </el-menu-item-group>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view />
+        </el-main>
+        <el-footer>
+          <div>
+            <div>版权所有&copy;</div>
+            <div>jay-app-cli:Email:756774388@qq.com</div>
+          </div>
+        </el-footer>
+      </el-container>
     </el-container>
   </el-container>
- 
 </template>
-
+<script>
+const imgUrl = require("../src/assets/avatar.jpeg");
+export default {
+  data() {
+    return {
+      avatarUrl: imgUrl,
+    };
+  },
+};
+</script>
 <style>
+.el-header {
+  background: #292961;
+  display: flex;
+  align-items: center;
+}
+.title {
+  color: white;
+  margin: auto;
+}
+.el-avatar > img {
+  width: 100%;
+  height: 100%;
+}
+.el-footer {
+  background: #e5e5e5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.el-aside {
+  background: #545c64;
+}
+.el-container {
+  height: calc(100vh - 80px);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
